4103763c88de6cae4d5d8795781f698a94be7071
ed <eduartesarno@gmail.com>
2025-12-27 22:28:45 -0800

    finish module 8, 2 more to go. This one was about iterators and its method that go inside of it

---
+++ course/iter/target/debug/deps/iter-3014696f350a5cbb.d
@@ -0,0 +1,5 @@
+/Users/eduardosarno/Documents/Code/Rust/course/iter/target/debug/deps/iter-3014696f350a5cbb.d: src/main.rs
+
+/Users/eduardosarno/Documents/Code/Rust/course/iter/target/debug/deps/libiter-3014696f350a5cbb.rmeta: src/main.rs
+
+src/main.rs:
